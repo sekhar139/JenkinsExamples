34b4815572b67ed2cf8407bb2ae6e8492327be72
sekhar139 <161412839+sekhar139@users.noreply.github.com>
2025-03-25 15:49:23 +0530

    Add files via upload

--- CommandLineDemo1.java
+++ CommandLineDemo1.java
@@ -9,10 +9,10 @@ r1=n1+n2;
 r2=n1-n2;
 r3=n1*n2;
 r4=n1/n2;
+
 System.out.println("Addition:"+r1);
-System.out.println("subtraction:"+r2);
+System.out.println("Subtraction:"+r2);
 System.out.println("Multiplication:"+r3);
 System.out.println("Division:"+r4);
 }
 }
-
